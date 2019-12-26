56c53039900dca771664135e8f63f420c8033d97
Opgohil <59160477+gohilbapu@users.noreply.github.com>
2019-12-26 08:39:06 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,7 +6,7 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "♔彡[ GUJJUOPGOHIL ]彡♔, check pinned in @opgohil"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "♔彡[ GUJJUOPGOHIL ]彡♔, check pinned in opgohil"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
