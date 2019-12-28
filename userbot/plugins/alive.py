af6ad4e300057f5b885efab6e9d6300bb60ff8a1
Opgohil <59160477+gohilbapu@users.noreply.github.com>
2019-12-29 01:20:18 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,17 +1,23 @@
-""".admin Plugin for @XtraTgBot"""
+""".admin Plugin for @UniBorg"""
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
-from platform import uname
-from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "♔彡[ GUJJUOPGOHIL ]彡♔, check pinned in opgohil"
 
-@command(outgoing=True, pattern="^.alive$")
-async def amireallyalive(alive):
-    """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI ψ(｀∇´)ψ`**\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @opgohil\n`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
-                     f"`My peru owner`: {opgohil}\n"
-                     "[Deploy this userbot Now](https://github.com/gohilbapu/X-tra-Telegram)")
+@borg.on(admin_cmd("alive"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "` Jinda Hu Sarr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\nfork by:` @GUJJUOPGOHIL\n`Database Status: Databases functioning normally!\n\n Always with you, my master! `"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
+
