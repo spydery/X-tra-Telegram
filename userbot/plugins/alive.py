07e0267ec24d806bc393c4bd5c92f05385319675
spydery <59336270+spydery@users.noreply.github.com>
2019-12-29 19:20:14 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -9,7 +9,7 @@ from userbot.utils import admin_cmd
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "` Jinda Hu opgohil Sarr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\nfork by:` @GUJJUOPGOHIL\n`Database Status: Databases functioning normally!\n\n Always with you, my master! `"
+    mentions = "` Jinda Hu opgohil Sarr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\nfork by:` @Spider_xD\n`Database Status: Databases functioning normally!\n\n Always with you, my master! `"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f""
